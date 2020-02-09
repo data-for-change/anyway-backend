0e49dce3e53ed822bfa644080891dd98bd9f6662
erangil <36845780+erangil@users.noreply.github.com>
2020-02-09 17:08:14 +0200

    aaa555

--- anyway/core/database.py
+++ anyway/core/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from . import config
 
 engine = create_engine(config.SQLALCHEMY_DATABASE_URI, convert_unicode=True, echo=False)
-engine.connect()
 Base = declarative_base()
 
 
