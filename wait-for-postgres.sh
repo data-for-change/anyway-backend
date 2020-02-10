88108633168d746badb16c2c452f08b54dd0a1f8
erangil <36845780+erangil@users.noreply.github.com>
2020-02-10 18:22:36 +0200

    aaa555

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -8,14 +8,17 @@ shift
 cmd="$@"
 
 until pg_isready -d $DATABASE_URL -q; do
-  >&2 echo "Postgres is not ready to recieve connections yet - sleeping"
+  >&2 echo "Postgres is not ready to receive connections yet - sleeping"
   sleep 10
 done
 
 >&2 echo "Postgres is up and running - executing migration command"
-exec $cmd
 if $cmd; then
+	echo before commit $ANYWAY_BACKEND_DB_IMG
+	docker ps
+	docker images
 	docker commit workspace_db_1 $ANYWAY_BACKEND_DB_IMG
+	echo after commit $ANYWAY_BACKEND_DB_IMG
 	docker ps
 	docker images
 else
