46a99a8f04dae14bbc3c4477dda24894b52d1f93
erangil <36845780+erangil@users.noreply.github.com>
2020-02-18 17:13:05 +0200

    aaa555

--- alembic-upgrade.sh
+++ alembic-upgrade.sh
@@ -4,4 +4,5 @@ set -e
 env
 gcloud container clusters get-credentials anyway-dev-cluster-1 --zone europe-west3-a --project anyway-backend
 POD=$(kubectl get pods -l app=my-app -o custom-columns=:metadata.name)
-kubectl exec $POD -c anyway-backend -- alembic upgrade head'
+echo $POD
+kubectl exec $POD -c anyway-backend -- alembic upgrade head
