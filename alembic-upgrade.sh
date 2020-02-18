0dc154915b1dd1c49aed241d3d1c59ef538be4e1
erangil <36845780+erangil@users.noreply.github.com>
2020-02-18 16:49:21 +0200

    aaa555

--- alembic-upgrade.sh
+++ alembic-upgrade.sh
@@ -3,5 +3,5 @@
 set -e
 env
 which kubectl
-POD = kubectl get pods -l app=anyway-backend -o custom-columns=:metadata.name
+export POD kubectl get pods -l app=anyway-backend -o custom-columns=:metadata.name
 kubectl exec $POD -c anyway-backend -- alembic upgrade head'
